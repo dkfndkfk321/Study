ef87dc6b284391c8dbc4cd86657edb9752b57591
dkfndkfk321 <48555199+dkfndkfk321@users.noreply.github.com>
2019-03-16 15:35:45 +0900

    총알 발사 관련 소스코드 개선

---
+++ Moblie Deffense Game/Assets/Scripts/BulletStat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStat
+{
+
+    public float speed { set; get; }
+    public int damage { set; get; }
+
+    public BulletStat(float speed, int damage)
+    {
+        this.speed = speed;
+        this.damage = damage;
+    }
+
+ 
+}
